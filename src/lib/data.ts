1e75c73b5395548d2cd2d674b2a5bb3937c9dafe
gracebir <grace.devolop@gmail.com>
2023-08-05 13:38:37 +0200

    nav element data

---
+++ src/lib/data.ts
@@ -0,0 +1,27 @@
+export const navElts = [
+    {
+        id: 1,
+        text: 'Home',
+        href: '/'
+    },
+    {
+        id: 2,
+        text: 'Menu',
+        href: '#'
+    },
+    {
+        id: 3,
+        text: 'How it works',
+        href: '#'
+    },
+    {
+        id: 4,
+        text: 'About',
+        href: '#'
+    },
+    {
+        id: 5,
+        text: 'Contact',
+        href: '#'
+    }
+]
