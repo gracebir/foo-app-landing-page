3e2df5e964579576256a6cb69930e9d7df232512
gracebir <grace.devolop@gmail.com>
2023-08-05 10:05:33 +0200

    button components

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -7,7 +7,7 @@ type buttonProps = {
 
 const Button = ({ text, isIconed = false }: buttonProps) => {
     return (
-        <button className='flex gap-2 lg:px-6 px-4 lg:py-2 py-1 bg-orange-color rounded-full duration-300'>
+        <button className='flex gap-2 lg:px-6 px-4 lg:py-2 py-1 bg-orange-color hover:shadow-lg rounded-full duration-300'>
             {isIconed && (<Image src={"/assets/sign-in.svg"} width={15} height={17} alt='sign' />)}
             <span className='text-sm text-head-text font-bold'>{text}</span>
         </button>
