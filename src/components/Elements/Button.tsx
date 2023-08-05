938b527f776c1b977db6520f6a62fce7a5c7be18
gracebir <grace.devolop@gmail.com>
2023-08-05 18:23:13 +0200

    adding option of add adding customer classes

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image'
 type buttonProps = {
     text: string
     isIconed?: boolean
+    className?: string
 }
 
-const Button = ({ text, isIconed = false }: buttonProps) => {
+const Button = ({ text, isIconed = false, className }: buttonProps) => {
     return (
-        <button className='flex gap-2 lg:px-6 px-4 lg:py-2 py-1 bg-orange-color hover:shadow-lg rounded-full duration-300'>
+        <button className={`${className} flex gap-2 lg:px-6 px-4 lg:py-2 py-1 bg-orange-color hover:shadow-lg rounded-full duration-300`}>
             {isIconed && (<Image src={"/assets/sign-in.svg"} width={15} height={17} alt='sign' />)}
             <span className='text-sm text-head-text font-bold'>{text}</span>
         </button>
